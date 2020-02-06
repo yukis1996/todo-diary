8fe2e1d790d6f479489c81120ad87ac82f42916b
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2020-02-06 16:34:56 +0900

    modal_window
    モーダルウィンドウの作成
    それに伴うCSSの調整

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,11 @@
 ActiveRecord::Schema.define(version: 2020_02_03_051000) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "prefecture", null: false
+    t.string "image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
