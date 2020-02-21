49305b2cc8a0c4bb4a4eeb5b4d8679063eb78f73
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2020-02-21 19:00:52 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,8 @@ set :keep_releases, 5
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    # invoke 'unicorn:restart'
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
   end
 end
